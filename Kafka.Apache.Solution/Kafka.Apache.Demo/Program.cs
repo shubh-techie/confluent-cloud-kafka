4c02ad5e310a68ed2441fc2b230af3721461c31f
Shubh Prabhat <shubh.techie@gmail.com>
2022-08-02 22:45:25 -0400

    Checking the remote code

---
+++ Kafka.Apache.Solution/Kafka.Apache.Demo/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//define producer properties 
+//create a producer 
+//send data
+//flush and close producer
